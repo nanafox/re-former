5aa7eb7c7ec94519e8a76d190c38bda0d91397cd
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-02 01:36:22 +0000

    feat: Add actions to edit, update and delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[update show edit destroy]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -7,9 +13,33 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to @user
     else
       render :new, status: :unprocessable_content
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.nil?
+      redirect_to root_path
+    else
+      redirect_to @user
+    end
+  end
+
+  def destroy
+    if @user.nil?
+      redirect_to users_path
+    else
+      @user.delete
+
+      redirect_to users_path, status: :see_other
     end
   end
 
@@ -17,5 +47,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.expect(user: [ :username, :email, :password, :password_confirmation ])
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
   end
 end
