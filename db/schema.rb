d16a12494a30c1216414c23ef75c0935655af2da
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-02 10:29:59 +0000

    feat: Update to use password_digest to store encrypted user passwords

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_01_202204) do
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
-    t.string "password", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email"
